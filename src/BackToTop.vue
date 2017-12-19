4c25ee7b8b02518e8655e3cf2947ad7f109cc194
Alexander Vysotsky <utlime@gmail.com>
2017-12-19 15:44:42 +0700

    Loop in IE
    Loop will happen when browser round a number upward to its nearest integer.
    e.g. 
    if currentScroll = 2
    then (currentScroll - (currentScroll / 5)) = 1.6
    In chrome it will rounded to 1, but in ie11 it will be rounded to 2
    So on next step currentScroll will equal 2 - it's loop

--- src/BackToTop.vue
+++ src/BackToTop.vue
@@ -40,7 +40,7 @@ export default {
       let currentScroll = document.documentElement.scrollTop || document.body.scrollTop
       if (currentScroll > 0) {
         window.requestAnimationFrame(window.smoothscroll)
-        window.scrollTo(0, currentScroll - (currentScroll / 5))
+        window.scrollTo(0, Math.floor(currentScroll - (currentScroll / 5)))
       }
     }
 
