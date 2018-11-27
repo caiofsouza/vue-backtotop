0d689d978a4092b313c59b9256d54ce65098b789
Caio Fernandes <caio_fsouza@hotmail.com>
2018-11-27 10:25:39 -0200

    add new dists

--- example/src/App.vue
+++ example/src/App.vue
@@ -47,7 +47,7 @@
 </template>
 
 <script>
-import BackToTop from '../../src/BackToTop.vue'
+import BackToTop from 'vue-backtotop'
 
 export default {
   name: 'app',
@@ -62,20 +62,20 @@ export default {
   },
   created () {
     // private
-    this.scrollIndentBackTop = 0
-    this.scrollHeight = 0
+    this.scrollIndentBackTop = 0;
+    this.scrollHeight = 0;
   },
   mounted () {
       this.scrollHeight = Math.max(
           document.body.scrollHeight, document.documentElement.scrollHeight,
           document.body.offsetHeight, document.documentElement.offsetHeight,
           document.body.clientHeight, document.documentElement.clientHeight
-      ) - window.innerHeight
-      this.scrollIndentBackTop = document.getElementsByClassName('footer')[0].clientHeight / 2
+      ) - window.innerHeight;
+      this.scrollIndentBackTop = document.getElementsByClassName('footer')[0].clientHeight / 2;
   },
   watch: {
     visibleoffset (newVal, oldVal) {
-      document.body.style.height = (parseInt(newVal) + 2000) + 'px'
+      document.body.style.height = (parseInt(newVal) + 2000) + 'px';
     }
   },
   methods: {
@@ -83,8 +83,8 @@ export default {
       console.log('Scrolled !')
     },
     scrollFn: function (eventObject) {
-      let diff = this.scrollHeight - window.pageYOffset
-      this.isBackTopFooter = diff < (this.scrollIndentBackTop - 40 - 15)
+      let diff = this.scrollHeight - window.pageYOffset;
+      this.isBackTopFooter = diff < (this.scrollIndentBackTop - 40 - 15);
     }
   },
   components: { BackToTop }
